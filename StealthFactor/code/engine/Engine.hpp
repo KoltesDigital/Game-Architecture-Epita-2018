KoltesDigital/Game-Architecture-Epita-2017

Add fake network + Add getClassName for serialization

StealthFactor/code/engine/Engine.hpp
@@ -7,6 +7,7 @@
 #include <engine/frontend/FrontendManager.hpp>
 #include <engine/graphics/GraphicsManager.hpp>
 #include <engine/input/InputManager.hpp>
+#include <engine/network/NetworkManager.hpp>
 
 namespace engine
 {
@@ -25,6 +26,7 @@ namespace engine
 
 		// EventListener
 		void onEvent(const sf::Event &event) override;
+		void onChat(const std::string &text) override;
 
 	private:
 		assets::Manager assetsManager;
@@ -32,6 +34,7 @@ namespace engine
 		frontend::Manager frontendManager;
 		graphics::Manager graphicsManager;
 		input::Manager inputManager;
+		network::Manager networkManager;
 
 		bool running;
 		float deltaTime;

KoltesDigital/Game-Architecture-Epita-2018

StealthFactor/code/engine/Engine.hpp
@@ -6,6 +6,7 @@
 #include <engine/frontend/FrontendManager.hpp>
 #include <engine/graphics/GraphicsManager.hpp>
 #include <engine/input/InputManager.hpp>
+#include <engine/network/NetworkManager.hpp>
 #include <engine/Configuration.hpp>
 #include <engine/EventListener.hpp>
 
@@ -26,6 +27,7 @@ namespace engine
 
 		// EventListener
 		void onEvent(const sf::Event &event) override;
+		void onChat(const std::string &text) override;
 
 	private:
 		assets::Manager _assetsManager;
@@ -33,6 +35,7 @@ namespace engine
 		frontend::Manager _frontendManager;
 		graphics::Manager _graphicsManager;
 		input::Manager _inputManager;
+		network::Manager _networkManager;
 
 		bool _running;
 		float _deltaTime;